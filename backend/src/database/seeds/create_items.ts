3a12051ee1749db8779ae8ed8b479fe9aa5c7e98
Rayron Rodffer <r.rodffer@gmail.com>
2020-10-08 01:18:10 -0300

    frontend

--- backend/src/database/seeds/create_items.ts
+++ backend/src/database/seeds/create_items.ts
@@ -3,10 +3,10 @@ import Knex from 'knex';
 export async function seed(knex: Knex) {
   await knex('items').insert([
     { title: 'Lâmpada', image: 'lampada.png'},
-    { title: 'Pilha e bateria', image: 'bateria.png'},
+    { title: 'Pilha e Bateria', image: 'bateria.png'},
     { title: 'Papel e Papelão', image: 'papel.png'},
-    { title: 'Resíduo eletrônico', image: 'eletronico.png'},
+    { title: 'Resíduo Eletrônico', image: 'eletronico.png'},
     { title: 'Resíduo Orgânico', image: 'organico.png'},
-    { title: 'Óleo alimentar', image: 'oleo.png'},
+    { title: 'Óleo Vegetal', image: 'oleo.png'},
   ])
 }
